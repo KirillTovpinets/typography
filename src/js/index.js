e8894321a283e381da3129376f165e2396ce5ff0
Kirill Tovpinec <kirilltovpinec@KirillTovpinets-Macbook-Air.local>
2019-02-08 12:47:41 +0300

    feat: add animation

--- src/js/index.js
+++ src/js/index.js
@@ -8,13 +8,45 @@ $(document).ready(function() {
 		return false;
 	})
 	
+	$('#flash').addClass('fadeOut');
+	setTimeout(() => {
+		$('#flash').hide();
+		$('.adv-item').addClass('fadeInUp')
+		$('.contact-form').addClass('fadeInRight')
+	}, 1000);
 	const map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: 53.912732780101635, lng: 27.569198115906357},
 			zoom: 15
 	});
+	$('[data-toggle="tooltip"]').tooltip({ boundary: 'window' })
 	// let marker = new google.maps.Marker({
 	// 		position: position,
 	// 		map: map,
 	// 		icon: 'img/marker.png'
 	// });
+
+	$(window).scroll(function(e){
+		let scrolled = $('html').scrollTop();
+		console.log(scrolled);	
+			if (scrolled > 300){
+				$('#services .title.animated').addClass('fadeInUp')
+				$('#services .service.animated').addClass('fadeInUp')
+			}
+			if (scrolled > 800){
+				$('#pricelist .title.animated').addClass('fadeInUp')
+				$('#pricelist .price-container.animated').addClass('fadeInUp')
+			}
+			if (scrolled > 1400){
+				$('#make-order .title.animated').addClass('fadeInUp')
+				$('#make-order .steps-container .animated').addClass('fadeInUp')
+			}
+			if (scrolled > 2600){
+				$('#skills .title.animated').addClass('fadeInUp')
+				$('#skills .skill-item.animated').addClass('fadeInUp')
+			}
+			if (scrolled > 3500){
+				$('#partners .title.animated').addClass('fadeInUp')
+				$('#partners .partner.animated').addClass('fadeIn')
+			}
+	})
 })
